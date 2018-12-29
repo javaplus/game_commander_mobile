eaf6c20a61b3ea70f6bcf7932713f4dfa04a58cf
Barry Tarlton <btarlton@gmail.com>
2018-12-29 11:07:14 -0500

    more tests to verify time interval creation

--- src/app/timerRequest/timer.service.spec.ts
+++ src/app/timerRequest/timer.service.spec.ts
@@ -38,4 +38,34 @@ describe('TimerService', () => {
         expect(gameRequest.speaktime.speakItems[0].say).toBe("5 minutes remaining. 5 minutes.");
 
     });
+
+    it('when setupGame called with gameTime of 5 and interval of 3, then speakItems created for 5 minutes and 3 minutes', ()=>{
+        let gameSetup = new GameSetup();
+        gameSetup.setupTime = 5;
+        gameSetup.gameTime = 5;
+        gameSetup.intervalTime = 3;
+        let gameRequest = service.setupGame(gameSetup);
+
+        expect(gameRequest.speaktime.speakItems.length).toBe(2);
+        expect(gameRequest.speaktime.speakItems[0].say).toBe("5 minutes remaining. 5 minutes.");
+        expect(gameRequest.speaktime.speakItems[1].say).toBe("2 minutes remaining. 2 minutes.");
+
+    });
+
+    
+    it('when setupGame called with gameTime of 25 and interval of 5, then speakItems created for 25, 20, 15,10, and 5 minutes', ()=>{
+        let gameSetup = new GameSetup();
+        gameSetup.setupTime = 3;
+        gameSetup.gameTime = 25;
+        gameSetup.intervalTime = 5;
+        let gameRequest = service.setupGame(gameSetup);
+
+        expect(gameRequest.speaktime.speakItems.length).toBe(5);
+        expect(gameRequest.speaktime.speakItems[0].say).toBe("25 minutes remaining. 25 minutes.");
+        expect(gameRequest.speaktime.speakItems[1].say).toBe("20 minutes remaining. 20 minutes.");
+        expect(gameRequest.speaktime.speakItems[2].say).toBe("15 minutes remaining. 15 minutes.");
+        expect(gameRequest.speaktime.speakItems[3].say).toBe("10 minutes remaining. 10 minutes.");
+        expect(gameRequest.speaktime.speakItems[4].say).toBe("5 minutes remaining. 5 minutes.");
+
+    });
 });
